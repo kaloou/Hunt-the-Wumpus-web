10c1974ecbec0ed292119c6b0aa33d48e25fc154
kalouu <lucaspalu866@gmail.com>
2026-02-09 11:37:58 +0100

    add constants to simplify the code

---
+++ game/constants.py
@@ -0,0 +1,25 @@
+##==========
+## CONSTANTS
+##==========
+ROW = 6
+COL = 8
+
+EASY = 1
+NORMAL = 2
+HARD = 3
+
+CAVE = 1
+TUNNEL = 2
+ULDRTUNNEL = 3
+URDLTUNNEL = 4
+PIT = 6
+BAT = 7
+WUMPUS = 8
+PLAYER = 9
+BLOOD = 10
+SLIME = 11
+
+UP = 1
+DOWN = -1
+LEFT = -2
+RIGHT = 2
